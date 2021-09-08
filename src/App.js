1dfb4dd71c380cdacb08a9c6b49b302fa0cafdd0
mehtaReena <j.reena07@gmail.com>
2021-09-08 22:53:45 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -126,7 +126,7 @@ setTodos(res.data.updateTodo)
       </form>
       <ul>
 
-       {  deleting ?  <h6 style={{color:'#17A2B8'}}> Procesing... </h6>:
+       {  deleting ?  <h6 style={{textAlign : 'center', color:'#17A2B8'}}> Procesing... </h6>:
 
        todos.map((todo) =>
           <li key={todo.id} style={{ width:"400px" ,padding:"5px"}}>
